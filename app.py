e7c953253dc307dc40950b7d08bc2d5802c07c6c
Matheus <matheus@bright.pt>
2023-02-06 18:18:59 -0300

    [feat] Search Artists

--- app.py
+++ app.py
@@ -78,7 +78,6 @@ with app.app_context():
     is_search = request.method == 'POST'
     search_term = request.form.get('search_term', '')
     results_count = 0
-    items = []
 
     if is_search:
       query = City.query.filter(exists().where(Venue.city_id == City.id).where(Venue.name.ilike(f'%{search_term}%')))
@@ -134,23 +133,17 @@ with app.app_context():
   @app.route('/artists/search', methods=['POST'])
   @app.route('/artists')
   def artists():
-    artists = Artist.query.order_by(Artist.name).all()
-    return render_template('pages/artists.html', artists=artists)
+    is_search = request.method == 'POST'
+    search_term = request.form.get('search_term', '')
+    results_count = 0
 
-  @app.route('/artists/search', methods=['POST'])
-  def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-    # search for "band" should return "The Wild Sax Band".
-    response={
-      "count": 1,
-      "data": [{
-        "id": 4,
-        "name": "Guns N Petals",
-        "num_upcoming_shows": 0,
-      }]
-    }
-    return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+    if is_search:
+      artists = Artist.query.filter(Artist.name.ilike(f'%{search_term}%')).order_by(Artist.name).all()
+      results_count = len(artists)
+    else:
+      artists = Artist.query.order_by(Artist.name).all()
+
+    return render_template('pages/artists.html', artists=artists, search_term=search_term, results_count=results_count)
 
   @app.route('/artists/<int:artist_id>')
   def show_artist(artist_id):
